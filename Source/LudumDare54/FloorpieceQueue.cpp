1e9a07629d984491c784c70eba0dd8ca7ae5d1cb
Marissa R.S <mrowles@academic.rrc.ca>
2023-09-30 14:05:36 -0500

    adding floorpieces

---
+++ Source/LudumDare54/FloorpieceQueue.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FloorpieceQueue.h"
+
+// Sets default values
+AFloorpieceQueue::AFloorpieceQueue()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AFloorpieceQueue::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AFloorpieceQueue::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
